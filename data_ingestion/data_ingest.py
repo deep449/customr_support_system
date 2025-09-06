00f257755a0e5ea9313cf077bcfa73cbf0d70ab6
deep449 <deepakdabi291@gmail.com>
2025-09-06 20:42:21 +0530

    skeleton created

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,13 @@
+from langchain_astradb import AstraDBVectorStore
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from data_ingestion.data_transform import data_converter
+
+
+class ingest_data:
+    def __init__(self):
+        pass
+
+    def data_ingestion(self):
+        pass
