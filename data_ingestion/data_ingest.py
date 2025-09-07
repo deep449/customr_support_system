240f209e31a91d136adc738f6c797515ad4dc9a9
deep449 <deepakdabi291@gmail.com>
2025-09-07 18:40:39 +0530

    env variable added

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -5,6 +5,17 @@ import pandas as pd
 from data_ingestion.data_transform import data_converter
 
 
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+ASTRA_DB_API_ENDPOINT=os.getenv("")
+ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_KEYSPACE=os.getenv("ASTRA_DB_KEYSPACE")
+
+
+
+
+
+
+
 class ingest_data:
     def __init__(self):
         print("data ingestion class has init....")
