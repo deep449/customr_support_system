aa4254d7774949648c5108e250500330149e9f51
deep449 <deepakdabi291@gmail.com>
2025-09-07 18:26:14 +0530

    code updated

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -7,11 +7,46 @@ class data_converter:
     def __init__(self):
         print("data converter has been init....")
         self.product_data=pd.read_csv("./data/flipkart_product_review.csv")
-        print(self.product_data.head())
+        #print(self.product_data.head())
 
     def data_transformation(self):
-        pass
+        required_columns=self.product_data.columns
+        required_columns=list(required_columns[1:])
+        #print(required_columns)
+
+        product_list = []
+
+        for index,row in self.product_data.iterrows():
+            object={
+                "product_name":row['product_title'],
+                "product_rating":row['rating'],
+                "product_summary":row['summary'],
+                "product_review":row['review']
+            }
+            product_list.append(object)
+
+        #print("************below is my product list**************")
+        #print(product_list)
+        docs = []
+        for entry in product_list:
+            metadata={"product_name":entry["product_name"],
+                      "product_rating":entry["product_rating"],
+                      "product_summary":entry["product_summary"]
+                      }
+            doc = Document(page_content=entry["product_review"],metadata=metadata)
+            docs.append(doc)
+            print(docs[0])
+
+        #return docs
+
+
+
+
+
 
 
 if __name__ == '__main__':
     data_con=data_converter()
+    data_con.data_transformation()
+
+
